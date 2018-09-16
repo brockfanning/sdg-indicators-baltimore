65e721fb5f1bac52fd3397b300b140344653d4ef
Brock Fanning <brockfanning@gmail.com>
2018-09-15 21:43:12 -0400

    Initial commit.

---
+++ _pages/contact.md
@@ -0,0 +1,9 @@
+---
+layout: page
+title: Contact
+permalink: /contact/
+---
+### We are interested in your feedback regarding this website and its contents.
+
+
+[content needed]
---
+++ scripts/deploy/circleci/deploy_prod.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+git config --global user.email $GH_EMAIL
+git config --global user.name $GH_NAME
+
+# CircleCI will identify the SSH key with a "Host" of gh-stg. In order to tell
+# Git to use this key, we need to hack the SSH key:
+sed -i -e 's/Host gh-prod/Host gh-prod\n  HostName github.com/g' ~/.ssh/config
+# Now we can use "gh-prod" below to identify the SSH key to use.
+git clone git@gh-prod:$GH_ORG_PROD/$GH_ORG_PROD.github.io.git out
+
+cd out
+git checkout master || git checkout --orphan master
+git rm -rfq .
+cd ..
+
+# The fully built site is already available at ~/repo/_site.
+cp -a ~/repo/_site/. out/.
+
+mkdir -p out/.circleci && cp -a .circleci/. out/.circleci/.
+cd out
+
+git add -A
+git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+
+git push origin master
